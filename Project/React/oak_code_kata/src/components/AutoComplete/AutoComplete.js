790569536d7361134001b66538d55859993d2dfb
Rainstriker <cloneltorrer@gmail.com>
2020-12-05 02:36:50 +0700

    Auto Complete Done!

--- Project/React/oak_code_kata/src/components/AutoComplete/AutoComplete.js
+++ Project/React/oak_code_kata/src/components/AutoComplete/AutoComplete.js
@@ -4,26 +4,44 @@ import CountryList from './CountryList.js';
 import { Form, Card } from 'react-bootstrap';
 
 const AutoComplete = () => {
-  const [term, setTerm] = useState('term');
+  const [term, setTerm] = useState('');
   const [found, setFound] = useState([]);
 
   const search = (e) => {
     const term = e.target.value;
+    setTerm(term);
     const found = Countries.filter(country =>
       country.toLowerCase().startsWith(term.toLowerCase())
     );
     term === '' ? setFound([]) : setFound(found);
-    setTerm(term);
+  }
 
+  const newTerm = term => {
+    setTerm(term);
+    setFound([]);
   }
 
   return (
-    <Form style={{margin: '0 auto'}}>
-      <Card style={{ width: '23.875rem', maxHeight: '30.125rem', minHeight: '9.300625rem', height: '100%'}}>
-        <h3>Auto Completed</h3>
-        <input type='text' onChange={search} />
-        <CountryList found={found} />
-      </Card>
+    <Form className='d-flex justify-content-center' style={{ marginTop: '50px'}}>
+      <div className='align-self-center'>
+        <Card className='p-3' style={{
+          width: '23.875rem',
+          maxHeight: '30.125rem',
+          minHeight: '9.300625rem',
+          height: '100%',
+          textAlign: 'center',
+          backgroundColor: '#F5F5F5',
+        }}>
+          <h4>Auto Completed</h4>
+          <input type='text' onChange={search} value={term}/>
+          <hr style={{ marginTop: 10, marginBottom: 0}}/>
+          <CountryList found={found} new={newTerm}/>
+        </Card>
+        <Card.Header className='rounded-bottom border border-warning'
+          style={{ width: '23.875rem', backgroundColor: '#FFD32E', height: '2.6875rem', textAlign: 'center' }}>
+          Suggestions: {found.length}
+        </Card.Header>
+      </div>
     </Form>
   );
 }
--- Project/React/oak_code_kata/src/components/AutoComplete/CountryList.js
+++ Project/React/oak_code_kata/src/components/AutoComplete/CountryList.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
 
 const CountryList = props => {
   return (
-    <ul style={{overflow: 'scroll'}}>
+    <div className='overflow-auto'>
       {
         props.found.map(country => 
-          <li key={country}>{country}</li>
+          <Card className='d-flex flex-row my-1' 
+                key={country} 
+                style={{height: '2.6875rem', backgroundColor: '#0000000D', cursor: 'pointer'}}
+                value={country}
+                onClick={() => props.new(country)} 
+                >
+            <Card.Header className='rounded-left border-right-0 p-1' style={{backgroundColor: '#FFD32E'}}></Card.Header>
+            <Card.Body className='p-0 m-auto'>
+              {country}
+            </Card.Body>
+          </Card>
           )
       }
-    </ul>
+    </div>
   );
 }
 
