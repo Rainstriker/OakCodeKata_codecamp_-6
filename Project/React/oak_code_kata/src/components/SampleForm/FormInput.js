24aff6b8440abc4e5704b06396af52fe88b6faa6
Rainstriker <cloneltorrer@gmail.com>
2020-12-05 22:09:40 +0700

    add FromInput

--- Project/React/oak_code_kata/src/components/SampleForm/FormInput.js
+++ Project/React/oak_code_kata/src/components/SampleForm/FormInput.js
@@ -3,14 +3,41 @@ import React from 'react';
 const FormInput = () => {
   return (
     <form>
-      <input type="text"/>
-      <input type="text"/>
-      <input type="text"/>
-      <input type="radio"/>
-      <input type="radio"/>
-      <h5>
-        Select your destination
-      </h5>
+      <div class="form-group">
+        <input type="text" /><br />
+        <input type="text" /><br />
+        <input type="text" /><br />
+      </div>
+      <br />
+      <div class="form-group">
+        <input type="radio" id="male" name="gender" value="male" />
+        <label for="male">Male</label><br />
+        <input type="radio" id="female" name="gender" value="female" />
+        <label for="female">Female</label><br />
+      </div>
+      <div class="form-group">
+        <label>
+          <h6>
+            Select your destination
+          </h6>
+        </label><br />
+        <select>
+          <option key="1">--Please Choose a destination --</option>
+          <option key="2">Thailand</option>
+          <option key="3">Japan</option>
+          <option key="4">Brazil</option>
+        </select>
+      </div>
+      <h6>Dietary restrictions:</h6>
+      <div class="form-group form-check">
+        <input type="checkbox" id="nutsFree" name="dietary1" value="nuts free" />
+        <label for="nutsFree" className="form-check-label">Nuts free</label><br />
+        <input type="checkbox" id="lactoseFree" name="dietary2" value="lactose free" />
+        <label for="lactoseFree" className="form-check-label">Lactose free</label><br />
+        <input type="checkbox" id="vegan" name="dietary3" value="vegan" />
+        <label for="vegan" className="form-check-label">Vegan</label><br />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
     </form>
   );
 }
--- Project/React/oak_code_kata/src/components/SampleForm/SampleForm.js
+++ Project/React/oak_code_kata/src/components/SampleForm/SampleForm.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import FormInput from 'FormInput.js'
+import FormInput from './FormInput.js'
 
 const SampleForm = () => {
   return (
     <div>
       <h1>Sample form</h1>
+      <FormInput />
     </div>
   );
 }
