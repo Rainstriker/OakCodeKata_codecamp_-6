5ae49352c0ce742fdc3581ec073126a327ea2b01
Rainstriker <cloneltorrer@gmail.com>
2020-12-09 01:32:54 +0700

    HTML project all done

--- Project/Bootstrap/Project 2 -  Web ข่าว/index.js
+++ Project/Bootstrap/Project 2 -  Web ข่าว/index.js
@@ -1,13 +1,13 @@
 //--------------------SIDENAV BUTTON--------------------------------------------
 
-function myFunction(x) {
+function toggleChange(x) {
     x.classList.toggle("change");
 }
 
 //--------------------SIDENAV--------------------------------------------------------------
 
 function toggleNav() {
-let element = document.getElementById("mySidenav");
+    let element = document.getElementById("mySidenav");
     if (element.style.width == "250px") {
         element.style.width = "0px";
         document.getElementById("mySearchIcon").style.width = "0px";
@@ -20,13 +20,13 @@ let element = document.getElementById("mySidenav");
 
 //----------------------DATE-------------------------------------------------------------
 
-var n =  new Date();
+var n = new Date();
 var year = n.getFullYear();
 var month = n.getMonth() + 1;
 var dayNum = n.getDate();
 var weekDayNum = n.getDay();
 const weekDay = (num) => {
-    switch(num) {
+    switch (num) {
         case 0:
             return "Sunday";
         case 1:
@@ -45,7 +45,7 @@ const weekDay = (num) => {
 }
 
 const monthName = (num) => {
-    switch(num) {
+    switch (num) {
         case 1:
             return "Jan";
         case 2:
@@ -78,3 +78,56 @@ const monthName = (num) => {
 const myMainSearch = () => {
     document.getElementById("mySearchIcon").style.width = "60px";
 }
+
+const navBarHide = () => {
+    const mainSearch = document.getElementById("main_search");
+    const mainNav = document.getElementById("news-nav-content2");
+    const arr = document.getElementsByClassName("main-nav-item");
+    //mainSearch.style.width = "100%"
+    //mainSearch.style.left = "1px"
+    mainNav.style.width = '0';
+    mainNav.style.transition = 'all 1s';
+
+    for (let i in arr) {
+        arr[i].style.display = "none";
+    }
+    
+}
+
+const navhBarShow = () => {
+    const mainSearch = document.getElementById("main_search");
+    const mainNav = document.getElementById("news-nav-content2");
+    const arr = document.getElementsByClassName("main-nav-item");
+    //mainSearch.style.width = "10px"
+    //mainSearch.style.left = "0px"
+    mainNav.style.transition = 'none';
+    mainNav.style.width = '70%';
+    setTimeout(() => {
+        mainNav.style.transition = 'all 0.5s';
+        mainNav.style.width = '95%';
+
+        for (let i in arr) {
+        arr[i].style.display = "block";
+        }
+    },10)
+    
+}
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+let arr = document.getElementById("menu-dropdown");
+let currentScrollPos = window.pageYOffset;
+const sideNav = document.getElementById("mySidenav");
+const icon = document.getElementById("mySearchIcon");
+
+  if (currentScrollPos > 180) {
+    document.getElementById("sticky").style.top = "0px";
+
+  } else {
+    document.getElementById("sticky").style.top = "-50px";
+    sideNav.style.width = "0px";
+    icon.style.width = "0px";
+    arr.classList.remove("change");
+  }
+}
+
